820b458d3c472ef5d2beac7109293e1fc9a3c5e0
Veer <veergajjar1201@gmail.com>
2026-06-17 11:36:20 -0700

    fixed brevo port num

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_SMTP_LOGIN,
     pass: process.env.BREVO_SMTP_PASSWORD
